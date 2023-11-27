af46ad5d178aca03f1ca2e22891427c7ce1a8596
DESKTOP\Angel Limas <angel.eliseo@gmail.com>
2023-11-26 21:34:37 -0600

    Added the logic in the canary test to get the driver with intellisense suggestions

--- uvdesk-framework/functional/tests/test_canary.py
+++ uvdesk-framework/functional/tests/test_canary.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append("../tests")
 from base_test import BaseTest
+from selenium import webdriver
 
 
 class TestCanary(BaseTest):
@@ -8,4 +9,6 @@ class TestCanary(BaseTest):
   def test_mycanary(self):
     logger = self.get_logger("canary_test")
     logger.info("Hello from canary")
+    local_driver: webdriver.Chrome = self.driver
+    logger.info(local_driver.title)
     assert True == True
